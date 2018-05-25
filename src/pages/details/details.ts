f1c92aa6f4bc80651fa7a6cd34ed0bec2285c359
Daniel Duque <dduque@blanco-brand.com>
2018-05-25 17:55:49 -0400

    add cordova, dev for android. Try to add cordova native google maps

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions
+} from '@ionic-native/google-maps';
+
 @IonicPage()
 @Component({
   selector: 'page-details',
@@ -10,10 +17,11 @@ import { RestProvider } from '../../providers/rest/rest';
 export class DetailsPage {
 	public code: string;
 	country: any;
+	map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestProvider) {
-  	this.code = navParams.get("code");
-  }
+  	constructor(public navCtrl: NavController, public navParams: NavParams, public rest: RestProvider) {
+  		this.code = navParams.get("code");
+  	}
 
 	ionViewDidLoad() {
 		this.getCountry();
@@ -22,7 +30,35 @@ export class DetailsPage {
 	getCountry(){
 		this.rest.getByCode(this.code).subscribe((country: any) => {
 			this.country = country;
+			this.loadMap();
 		});
 	}
+
+	loadMap(){
+		let mapOptions: GoogleMapOptions = {
+	    	camera: {
+	      		target: {
+	        		lat: this.country.latlng[0],
+	        		lng: this.country.latlng[1]
+	      		},
+	      		zoom: 18,
+	      		tilt: 30
+	    	}
+  		};
+
+  		this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+  		// Wait the MAP_READY before using any methods.
+  		this.map.one(GoogleMapsEvent.MAP_READY)
+  			.then(() => {
+    			// Now you can use all methods safely.
+    			console.log("ready");
+  			})
+  			.catch(error =>{
+    			console.log(error);
+  			});
+	}
+
+
 
 }
